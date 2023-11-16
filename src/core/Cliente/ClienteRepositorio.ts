d0eaa003075766a7a7943dbf9e7fd314976950e6
KingW3n <wendeljr01@gmail.com>
2023-11-16 17:39:19 -0300

    Integrando Cadastro com Firebase

--- src/core/Cliente/ClienteRepositorio.ts
+++ src/core/Cliente/ClienteRepositorio.ts
@@ -2,7 +2,7 @@
 import Cliente from "./Cliente";
 
 export default interface ClienteRepositorio{
-    salva(cliente:Cliente): Promise<Cliente>
+    salvar(cliente:Cliente): Promise<Cliente>
     excluir(cliente:Cliente): Promise<void>
     obterTodos():Promise<Cliente[]>
 }
